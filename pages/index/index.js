565ecc36af3681e56cda702cee45a22a77038680
mikuzip01 <52843828+mikuzip01@users.noreply.github.com>
2020-09-21 23:22:44 +0800

    mk1型程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,51 +1,176 @@
-const uploadFileUrl = 'https://127.0.0.1'  //yoursite是你申请的域名。
+//index.js
+//获取应用实例
+const app = getApp()
 
+Page({
+  data: {
+    motto: '所属区域：',
+    value: 'unknown',
+    p1: 0,
+    p2: 0,
+    resurl: '',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    img_arr: [],
+    condition: false,
+  },
 
-  Page({
-    data: {
-      imageSrc: null,
-    },
-    chooseImage: function () {  //绑定的chooseImage控件
-      var that = this
-      wx.chooseImage({ // 选定图片
-        sourceType: ['camera', 'album'],
-        sizeType: ['compressed'],  //这里选择压缩图片
-        count: 1,
-        success: function (res) {
-          console.log(res)
+  objectValueNotNone: function (obj) {  // 判断对象是否为空
+    for (var objKey in obj) {
+      if (!obj[objKey]) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  //事件处理函数
+  bindViewTap: function() {
+    var that = this //！！！！！！！！！“搭桥”
+
+    //利用API从本地读取一张图片
+    wx.chooseImage({
+      count: 1,
+      //sizeType: ['original', 'compressed'],
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths
+        //将读取的图片替换之前的图片
+        that.setData({
+          userImage: tempFilePaths[0],
+          img_arr: that.data.img_arr.concat(tempFilePaths[0]),
+          condition: false,
+        }) //通过that访问
+        console.log(that.data.userImage)
+      }
+    })
+  },
+  changeName: function(e) {
+    this.setData({
+      value: "xiao",
+    })
+
+  },
+  upload: function() {
+    var that = this
+    // loading检测结果，这里加上mask防止用户重复点击，目前设定的超时时间为30秒
+    wx.showLoading({
+      title: '位置信息检测中',
+      mask: true
+    });
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 30000)
+    wx.uploadFile({
+      url: 'http://127.0.0.1:443/postdata',
+      // url: 'https://mikuzip01.zicp.io:443/postdata',
+      // filePath: that.data.img_arr[0],
+      filePath: that.data.userImage,
+      name: 'content',
+      // formData: adds,
+      success: function(res) {
+        console.log(res.data);
+        if(JSON.parse(res.statusCode) == 200){
           that.setData({
-            imageSrc: res.tempFilePaths[0]
+            value: JSON.parse(res.data)['value'],
+            userImage: JSON.parse(res.data)['resurl'],
+            resurl: JSON.parse(res.data)['resurl'],
+            p1: JSON.parse(res.data)['p1'],
+            p2: JSON.parse(res.data)['p2']
+          })
+  
+          if (res) {
+            wx.showToast({
+              title: '检测完成！',
+              duration: 3000
+            });
+          }
+        
+          that.setData({
+            condition:true,
           })
         }
-      })
-    },
-    check: function (e) {  // 绑定的check button
-      var that = this
-      wx.uploadFile({  // 上传图片
-        url: uploadFileUrl,
-        name: 'picture',
-        filePath: that.data.imageSrc,
-        formData: {
-          'user': 'test'
-        },
-        success: function (res) {
-          console.log('imageSrc is:', that.data.imageSrc)
-          console.log('uploadImage success, res is:', res)
-          wx.showModal({
-            title: "图片详情",
-            content: res.data,
-            showCancel: false,
-            confirmText: "确定"
-          })
-        },
-        fail: function ({ errMsg }) {
-          console.log('uploadImage fail, errMsg is', errMsg)
+        else{
+          wx.showToast({
+            title: '后台忙，请重新提交',
+            icon: "none",
+            duration: 3000
+          });
         }
-      })
-    },
-    reload: function (e) {  // 绑定的reload button
+        
+      }
+    })
+    this.setData({
+      formdata: ''
+    })
+  },
+  takePhoto() {
+    var that = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      //sizeType: ['original', 'compressed'],
+      sizeType: ['compressed'],
+      sourceType: ['camera'],
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          userImage: res.tempFilePaths[0],
+
+        })
+        console.log("res.tempImagePath" + tempFilePaths[0])
+      }
+    })
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
       this.setData({
-        imageSrc: null
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
       })
     }
-  })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  //点击事件  
+  // clickImg: function (e) {
+  //   var imgUrl = this.data.resurl;
+  //   if (this.data.condition==true){
+  //     wx.previewImage({
+  //       urls: [imgUrl], //需要预览的图片http链接列表，注意是数组
+  //       current: '', // 当前显示图片的http链接，默认是第一个
+  //       success: function (res) { },
+  //       fail: function (res) { },
+  //       complete: function (res) { },
+  //     })
+  //   }
+  // }
+
+})
